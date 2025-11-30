7d5554bde0ab23633fcdc51a89671c7c35201baf
Justin <JUSTINKJH06.23@ichat.sp.edu.sg>
2025-11-30 17:31:30 +0800

    Returned values added in bmi

--- bmi.py
+++ bmi.py
@@ -1,20 +1,18 @@
-def display_main_menu():
-    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+def calculate_bmi():
+    weight = 50
+    height = 1.85
+    bmi = weight/(height*height)
 
+    if bmi < 18.5:
+        print("Under Weight")
+        return -1
 
-def get_user_input():
-    user_input = input()
-    list_str = user_input.split(",")
-    list_float = [float(x) for x in list_str]
-    return list_float
+    elif 18.5 <= bmi <= 25.0:
+        print("Normal Weight")
+        return 0
 
-
-def calc_average_temperature(list_of_temps):
-    average = sum(list_of_temps) / len(list_of_temps)
-    return average
-
-
-def calc_min_max_temperature(list_of_temps):
-    min_temp = min(list_of_temps)
-    max_temp = max(list_of_temps)
-    return [min_temp, max_temp]
+    elif bmi>25.0:
+        print("Over weight")
+        return 1
+    
+print(calculate_bmi())
